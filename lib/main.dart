943ec1a6fca4a08a02895c91d3e5e1c2e215c1bf
Turazzi <analivia.turazzi@gmail.com>
2026-04-26 22:18:23 -0300

    adiciona o aplicativo

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,261 @@
-import 'package:flutter/material.dart';
+  import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+  void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  class MyApp extends StatelessWidget {
+    const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        title: "Calculadora de Média de Notas",
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.blue,
+            foregroundColor: Colors.white,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.blue,
+              foregroundColor: Colors.white,
+            ),
+          ),
+        useMaterial3: true
+        ),
+        home: const CalculadoraMediaPage(),
+      );
+    }
   }
-}
+
+  class CalculadoraMediaPage extends StatefulWidget {
+    const CalculadoraMediaPage({super.key});
+
+    @override
+    State<CalculadoraMediaPage> createState() => _CalculadoraMediaPageState();
+  }
+
+  class _CalculadoraMediaPageState extends State<CalculadoraMediaPage> {
+
+    final _formKey = GlobalKey<FormState>();
+
+    final TextEditingController nomeController = TextEditingController();
+    final TextEditingController emailController = TextEditingController();
+    final TextEditingController nota1Controller = TextEditingController();
+    final TextEditingController nota2Controller = TextEditingController();
+    final TextEditingController nota3Controller = TextEditingController();
+
+    String nomeExibido = "";
+    String emailExibido = "";
+    String notasExibidas = "-  -";
+    String mediaExibida = "";
+
+    void calcularMedia() {
+      if (_formKey.currentState!.validate()) {
+
+        double n1 = double.parse(nota1Controller.text.replaceAll(',', '.'));
+        double n2 = double.parse(nota2Controller.text.replaceAll(',', '.'));
+        double n3 = double.parse(nota3Controller.text.replaceAll(',', '.'));
+
+        double media = (n1 + n2 + n3) / 3;
+
+        setState(() {
+          nomeExibido = nomeController.text;
+          emailExibido = emailController.text;
+          notasExibidas = "${n1.toStringAsFixed(1)} - ${n2.toStringAsFixed(1)} - ${n3.toStringAsFixed(1)}";
+          mediaExibida = media.toStringAsFixed(1);
+        });
+      }
+    }
+
+    void limparCampos() {
+      _formKey.currentState?.reset();
+
+      nomeController.text = '';
+      emailController.text = '';
+      nota1Controller.text = '';
+      nota2Controller.text = '';
+      nota3Controller.text = '';
+
+      setState(() {
+        nomeExibido = "";
+        emailExibido = "";
+        notasExibidas = "-  -";
+        mediaExibida = "";
+      });
+    }
+
+    @override
+    Widget build(BuildContext context) {
+
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text("Calculadora de Média de Nota"),
+        ),
+
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Text(
+                  'CALCULADOR DE MÉDIA',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                const SizedBox(height: 15),
+
+                TextFormField(
+                  controller: nomeController,
+                  decoration: const InputDecoration(labelText: 'Nome', border: OutlineInputBorder()),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) return 'Digite o nome';
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 10),
+
+                TextFormField(
+                  controller: emailController,
+                  decoration: const InputDecoration(labelText: 'Email', border: OutlineInputBorder()),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) return 'Digite o email';
+                    if (!value.contains('@')) return 'Email inválido';
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 10),
+
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: TextFormField(
+                        controller: nota1Controller,
+                        decoration: const InputDecoration(labelText: 'Nota 1', border: OutlineInputBorder()),
+                        keyboardType: TextInputType.number,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) return 'Vazio';
+                          String valTratado = value.replaceAll(',', '.');
+                          double? nota = double.tryParse(valTratado);
+                          if (nota == null) return 'Inválido';
+                          if (nota > 10) return 'Máx 10';
+                          if (nota < 0) return 'Mín 0';
+                          return null;
+                        },
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: TextFormField(
+                        controller: nota2Controller,
+                        decoration: const InputDecoration(labelText: 'Nota 2', border: OutlineInputBorder()),
+                        keyboardType: TextInputType.number,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) return 'Vazio';
+                          String valTratado = value.replaceAll(',', '.');
+                          double? nota = double.tryParse(valTratado);
+                          if (nota == null) return 'Inválido';
+                          if (nota > 10) return 'Máx 10';
+                          if (nota < 0) return 'Mín 0';
+                          return null;
+                        },
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    Expanded(
+                      child: TextFormField(
+                        controller: nota3Controller,
+                        decoration: const InputDecoration(labelText: 'Nota 3', border: OutlineInputBorder()),
+                        keyboardType: TextInputType.number,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) return 'Vazio';
+                          String valTratado = value.replaceAll(',', '.');
+                          double? nota = double.tryParse(valTratado);
+                          if (nota == null) return 'Inválido';
+                          if (nota > 10) return 'Máx 10';
+                          if (nota < 0) return 'Mín 0';
+                          return null;
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+
+                ElevatedButton(
+                  onPressed: calcularMedia,
+                  child: const Text('CALCULA MÉDIA'),
+                ),
+                const SizedBox(height: 20),
+
+                const Text('Resultado:', style: TextStyle(fontSize: 16)),
+                Text.rich(
+                  TextSpan(
+                    style: const TextStyle(fontSize: 16, color: Colors.black),
+                    children: [
+                      const TextSpan(text: 'Nome: '),
+                      TextSpan(
+                        text: nomeExibido,
+                        style: const TextStyle(fontWeight: FontWeight.bold), 
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 5),
+
+                Text.rich(
+                  TextSpan(
+                    style: const TextStyle(fontSize: 16, color: Colors.black),
+                    children: [
+                      const TextSpan(text: 'Email: '),
+                      TextSpan(
+                        text: emailExibido,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 5),
+
+                Text.rich(
+                  TextSpan(
+                    style: const TextStyle(fontSize: 16, color: Colors.black),
+                    children: [
+                      const TextSpan(text: 'Notas: '),
+                      TextSpan(
+                        text: notasExibidas,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 5),
+
+                Text.rich(
+                  TextSpan(
+                    style: const TextStyle(fontSize: 16, color: Colors.black),
+                    children: [
+                      const TextSpan(text: 'Média: '),
+                      TextSpan(
+                        text: mediaExibida,
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+
+                ElevatedButton(
+                  onPressed: limparCampos,
+                  child: const Text('APAGA OS CAMPOS'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+    }
+  }
